7fa2c63752bff7868a6df6f2a47323edd3131b7c
Romanovsky <masterofsea147@gmail.com>
2020-04-05 05:09:23 +0300

    MemoizeRealization

---
+++ DelegatesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConcurrencyWizardLibrary
+{
+    public static class DelegatesExtensions
+    {
+        public static Func<A, C> FromFuncComposeHof<A, B, C >
+            (this Func<A, B> f, Func<B, C> g)
+        {
+            return n => g(f(n));   
+        }        
+    }
+
+
+}
---
+++ MemoizeHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcurrencyWizardLibrary
+{
+    class MemoizeHelper
+    {
+        public static Func<T, R> Memoize<T, R>(Func<T, R> func)
+            where T : IComparable
+        {
+            ConcurrentDictionary<T, R> cache = new ConcurrentDictionary<T, R>();
+
+            return arg =>
+            {
+                if (!cache.ContainsKey(arg))
+                    cache.TryAdd(arg, func(arg));
+
+                return cache[arg];
+            };
+        }
+
+
+    }
+}
